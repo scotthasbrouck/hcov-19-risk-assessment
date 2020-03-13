fc694050982244c61cdb2e0f295761386e48073b
Scott Hasbrouck <scotthasbrouck@gmail.com>
2020-03-12 19:22:56 -0700

    initial commit

---
+++ precache-manifest.df4bcbdb31d5e3e61b2a1ec7e2ac84dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "919cde6dcd2c6cdf9cad129f0c793728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba86383e0013bfb585b5",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4ba33c09312b73750d85",
+    "url": "/static/js/2.ae7fca7e.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.ae7fca7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba86383e0013bfb585b5",
+    "url": "/static/js/main.d19992c4.chunk.js"
+  },
+  {
+    "revision": "41d73fad38d9a1b564c4",
+    "url": "/static/js/runtime-main.b58fa71d.js"
+  },
+  {
+    "revision": "6d3140ad48cae94b81b125c7f9868dd3",
+    "url": "/static/media/cov_icon.6d3140ad.webp"
+  }
+]);
